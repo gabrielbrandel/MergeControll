c481e517e64ae2febbc35ddd39c78fb1a55ecb4d
Gabriel Brandel <gabrielbrandel@gmail.com>
2024-11-08 16:36:35 -0300

    feat - Docker File

--- obj/Debug/net8.0/MergeControll.AssemblyInfo.cs
+++ obj/Debug/net8.0/MergeControll.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MergeControll")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46521b918e3d643c28384805de965c7e90556bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d6c9caf59c335a52a001fbac61be4f364fb5c53")]
 [assembly: System.Reflection.AssemblyProductAttribute("MergeControll")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MergeControll")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
